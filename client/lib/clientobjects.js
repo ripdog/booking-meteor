3908e44edd0d7888e484c64aa91848466cc7598f
Mitchell Ferguson <mitchell@ferguson.geek.nz>
2014-08-31 05:39:42 +0000

    Localtime now used globally.

--- client/lib/clientobjects.js
+++ client/lib/clientobjects.js
@@ -1,6 +1,3 @@
 Meteor.startup(function() {
-	
 }
 );//moment .isSame(obj, 'day')
-initialDate = moment().second(0).hour(0).minute(0).millisecond(0)._d;
-AutoForm.debug();
--- client/views/appointmentViewer.js
+++ client/views/appointmentViewer.js
@@ -1,37 +1,36 @@
-Template.bookinglist.helpers({
-	width: "700px"
-});
+Template.bookinglist.width = function() {return "700px";};
 Template.bookinglist.height = function() {return Session.get("height");}
 Template.bookinglist.rendered = render;
-// Template.bookinglist.autorun = render;
-// console.log("Starting query build.")
-// var theDate = Session.get("date");
-// console.log(theDate);
-// startDate = moment(theDate).startOf("day").toDate();
-// console.log(startDate);
-// endDate = moment(theDate).endOf("day").toDate();
-// console.log(endDate);
-// queryPointer = collections.apptList.find({date: {$gte: startDate, $lt: endDate}})
-// console.log(queryPointer.fetch());
+Template.bookinglist.autorun = render;
+
+Session.set("height", 1200);
 // moment();
 //appointmentList.find({date: Session.get("date")})
 function render(comp){
-// 	Session.set("height", 1200);
 	///////////////////////////////////////////////
 	///////////QUERY THE DATA
 	///////////////////////////////////////////////
+	console.log("Starting query build.")
+	var theDate = Session.get("date");
+	console.log(theDate);
+	startDate = moment(theDate).zone(-12).startOf("day")._d;
+	console.log(startDate);
+	endDate = moment(theDate).zone(-12).endOf("day")._d;
+	console.log(endDate);
+	console.log(JSON.stringify({date: {$gte: startDate, $lt: endDate}}))
+	queryPointer = appointmentList.find({date: {$gte: startDate, $lt: endDate}})
+	console.log(queryPointer.fetch());
 	///////////////////////////////////////////////
 	///////////RENDER
 	///////////////////////////////////////////////
-	Session.get("height");
 	this.autorun = Deps.autorun(function() {
-		var pxPerMinute = Math.ceil(Session.get("height")/((Session.get("endTime").getHours() -
-						Session.get("startTime").getHours())*60));//Basis of our pixel counting.
+		var pxPerMinute = Math.ceil(Session.get("height")/((Session.get("endTime") -
+						Session.get("startTime"))*60));//Basis of our pixel counting.
 		console.log("Px Per Minute: " + pxPerMinute);
 		canvas = document.getElementById('bookinglistcanvas');
 		var ctx = canvas.getContext('2d');
 		var lineCounter = (pxPerMinute * Session.get("appntlength"));
-		var textCounter = moment(Session.get("startTime"));
+		var textCounter = moment(Session.get("startTime"), "H");
 		while (true) {//first, draw the guidelines.
 			ctx.beginPath();
 			ctx.moveTo(0,lineCounter);
@@ -46,7 +45,6 @@ function render(comp){
 		}
 	})
 // 	console.log(comp)
-	
 // 	for (var booking in queryPointer.fetch())
 // 	{
 // 		console.log(booking);
